340f9e6939b8eacbb1d478ca2b6ae8192f2b386c
gabrielpasini <gabrielpasini@outlook.com.br>
2020-12-24 02:51:51 -0300

    :lipstick: favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import "../styles.css";
+import '../styles.css';
+import Head from 'next/head';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
