06917ed0c7c6844c8c233da6ed26405402e6f50b
Gabriel Pasini <gabrielpasini@outlook.com.br>
2024-10-26 01:20:12 -0300

    Update utils.js

--- public/utils.js
+++ public/utils.js
@@ -196,7 +196,7 @@ export const projects = [
   },
   {
     icon: 'https://drive.google.com/file/d/19AZ4ldE-V_V5yMy-ClRaYrvarL2lvA4P/view?usp=drivesdk',
-    name: 'Draginn'Up',
+    name: "Draginn'Up",
     description:
       'Jogo criado em 45 horas para participar da etapa regional da gamejam+.',
     platforms: [
